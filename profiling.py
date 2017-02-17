0274b0875f0ccbe5fb907814bb232c1160f96453
weidwonder <1026621841@qq.com>
2017-02-17 14:08:10 +0800

    [REV] Update time profiling utils.

--- profiling.py
+++ profiling.py
@@ -1,7 +1,10 @@
 import datetime as dt
 import timeit
+import types
 from functools import wraps
 from operator import itemgetter
+
+from django.views.generic import View
 
 
 class TimingManager(object):
@@ -81,12 +84,25 @@ def timing(func):
 
 def show_timing_cost(detail=False):
     func_cost_tuples = sorted(timing_dict.items(), key=lambda _: _[1]['total_costs'], reverse=True)
+    total_cost = sum([_[1]['total_costs'].total_seconds() for _ in func_cost_tuples])
+    print '%(func)-30s:%(total_secs)-12s%(call_times)-12s%(percent)s' % dict(func=u'method_name', total_secs=u'total_cost', 
+                                                                            call_times=u'times_run', percent=u'percent')
     for k, v in func_cost_tuples:
         total_seconds = v['total_costs'].total_seconds()
-        print '%(func)-30s:\t%(total_secs)s\t%(call_times)s' % dict(func=k, total_secs=total_seconds, call_times=v['times'])
+        percent = round(total_seconds / total_cost * 100, 2)
+        print '%(func)-30s:%(total_secs)-12s%(call_times)-12s%(percent)s' % dict(func=k, total_secs=total_seconds, 
+                                                                                call_times=v['times'], percent=percent)
         if detail:
             for i, time_cost in enumerate(v['time_costs']):
                 params = v['params'][i]
                 params_str = ', '.join([str(_) for _ in params[0]]) + ', ' + ', '.join(
                     [str(k) + '=' + repr(v) for k, v in params[1]])
                 print '\t - %(time_cost)s\t -%(params)s' % dict(time_cost=time_cost.total_seconds(), params=params_str)
+
+
+def view_timing(view):
+    for attr_name in set(dir(view)) - set(dir(View)):
+        attr = getattr(view, attr_name)
+        if isinstance(attr, types.MethodType):
+            setattr(view, attr_name, timing(attr))
+    return view
