c37574ac1aef19b300f774a2ee5ed06c2bcb113e
weidwonder <1026621841@qq.com>
2017-02-20 18:19:22 +0800

    [REV] tmp commit.

--- profiling.py
+++ profiling.py
@@ -1,29 +1,58 @@
 import datetime as dt
 import inspect
+import operator
 from collections import defaultdict
 from functools import wraps
+from threading import local
 
-import operator
 import types
 from django.views.generic import View
 
-tree = defaultdict(lambda : timing_dict)
+tree = defaultdict(lambda: tree, children=[])
 
 timing_dict = tree
 
-running_stack = []
-running_sets = set()
+_thread_locals = local()
+
+def Node(id, name, children=None, cost=0):
+    children = children or []
+    return {
+        'id': id,
+        'name': name,
+        'children': children,
+        'cost': cost,
+    }
+
+root = Node(0, 'root')
+
+nodes_map = {}
+def make_node(fn):
+    node = Node(id(fn), fn.__name__)
+    nodes_map[id(fn)] = node
+    return node
+
+
+def get_stack():
+    if not hasattr(_thread_locals, 'cur_stack'):
+        _thread_locals.cur_stack = [root]
+    return _thread_locals.cur_stack
+
 
 def timing(func):
     @wraps(func)
     def wrap_func(*args, **kws):
-        fn = func.__name__
-        start = dt.datetime.now()
+        stack = get_stack()
+        node = make_node(func)
+        parent = stack[-1]
+        parent['children'].append(node)
+        stack.append(node)
         func_stack = map(operator.itemgetter(3), inspect.stack())
-        running_stack.append(fn)
-        running_sets.add()
+
+        start = dt.datetime.now()
         rtn = func(*args, **kws)
         end = dt.datetime.now()
+
+        running_sets.add()
         time_cost = end - start
         timing_dict.setdefault(fn, {'times': 0, 'time_costs': [], 'params': [], 'total_costs': dt.timedelta(0)})
         timing_dict[fn]['times'] += 1
@@ -38,13 +67,13 @@ def timing(func):
 def show_timing_cost(detail=False):
     func_cost_tuples = sorted(timing_dict.items(), key=lambda _: _[1]['total_costs'], reverse=True)
     total_cost = sum([_[1]['total_costs'].total_seconds() for _ in func_cost_tuples])
-    print '%(func)-30s:%(total_secs)-12s%(call_times)-12s%(percent)s' % dict(func=u'method_name', total_secs=u'total_cost', 
-                                                                            call_times=u'times_run', percent=u'percent')
+    print '%(func)-30s:%(total_secs)-12s%(call_times)-12s%(percent)s' % dict(func=u'method_name', total_secs=u'total_cost',
+                                                                             call_times=u'times_run', percent=u'percent')
     for k, v in func_cost_tuples:
         total_seconds = v['total_costs'].total_seconds()
         percent = round(total_seconds / total_cost * 100, 2)
-        print '%(func)-30s:%(total_secs)-12s%(call_times)-12s%(percent)s' % dict(func=k, total_secs=total_seconds, 
-                                                                                call_times=v['times'], percent=percent)
+        print '%(func)-30s:%(total_secs)-12s%(call_times)-12s%(percent)s' % dict(func=k, total_secs=total_seconds,
+                                                                                 call_times=v['times'], percent=percent)
         if detail:
             for i, time_cost in enumerate(v['time_costs']):
                 params = v['params'][i]
--- test/test_stack.py
+++ test/test_stack.py
@@ -3,31 +3,39 @@ import operator
 
 from async_iter import AsyncIterHandler
 
-from django_tools.serializer import jprint
+from django_tools.serializer import jprint, to_json
 
 multitasking = AsyncIterHandler('threading')
+
 
 def print_stack():
     jprint(map(operator.itemgetter(3), inspect.stack()))
 
+
 class C(object):
-    def f(self):
-        task_list = []
-        for i in range(10):
-            task_list.append((self.g,) )
-        multitasking(task_list)
+    """ haha
+    """
 
+    x = y = 1
+
+    _g = 2
+
+    @property
     def g(self):
-        import threading
-        print threading._active
-        multitasking({
-            '1': (print_stack, )
-        })
+        return 1
 
 
-    def write(self, obj):
-        print obj
+lines = inspect.getsourcelines(C)
+print to_json(lines[0])
 
 
-c = C()
-c.f()
+def getdefinedattrs(object):
+    """ get class attrs list
+
+    Return attrs are defined in class despite whether those attrs 
+    are inherited from bases or not.
+    """
+    if not inspect.isclass(object):
+        raise TypeError('{!r} is not a class'.format(object))
+    lines = inspect.getsourcelines(C)
+    return ''
